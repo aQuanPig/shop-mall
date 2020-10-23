f50bd7a68fd29f4cc8d3c3f54963bd7152aeefce
ZZ <49825170+aQuanPig@users.noreply.github.com>
2020-10-23 17:51:29 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 export function request(config) {
   const instance = axios.create({
     // 需要接口的请加coderwhy老师
-    baseURL:''
+    baseURL:'http://123.207.32.32:8000/api/h8'
   })
   //请求拦截
   instance.interceptors.request.use(config=>{
